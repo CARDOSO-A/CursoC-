263fe50d8aabfd3e7fb085db94eb456650abc0aa
Antonio Cardoso <antonioc_contas@hotmail.com>
2022-01-03 15:38:15 -0300

    Estrutura de Controle
    Término do capítulo de estrutura de controle do C #.

---
+++ EstruturasDeControle/EstruturaForEach.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle {
+    internal class EstruturaForEach {
+
+        public static void Executar() {
+            var palavra = "Opa!";
+
+            foreach(var letra in palavra) {
+                Console.WriteLine(letra);
+            }
+
+            var alunos = new string[] { "Anna", "Antonio", "Emma" };
+
+            foreach (string row in alunos) {
+                Console.WriteLine(row);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturasDeControle;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+
+                //Fundamentos
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar}, //nome_classe.nomemetodo
                 {"Comentários - Fundamentos", Comentarios.Executar},
                 {"Váriaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
@@ -21,6 +24,18 @@ namespace CursoCSharp {
                 {"Operadores Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
                 {"Operadores Unarios - Fundamentos", OperadoresUnarios.Executar},
                 {"Operador Ternario - Fundamentos", OperadorTernario.Executar},
+
+                //Estruturas de Controle
+                {"Estrutura If - Estruturas de Controle", EstruturaIf.Executar},
+                {"Estrutura If/Else - Estruturas de Controle", EstruturaIfElse.Executar},
+                {"Estrutura If/Else/If - Estruturas de Controle", EstruturaIfElseIf.Executar},
+                {"Estrutura Switch - Estruturas de Controle", EstruturaSwitch.Executar},
+                {"Estrutura While - Estruturas de Controle", EstruturaWhile.Executar},
+                {"Estrutura Do While - Estruturas de Controle", EstruturaDoWhile.Executar},
+                {"Estrutura For - Estruturas de Controle", EstruturaFor.Executar},
+                {"Estrutura For Each - Estruturas de Controle", EstruturaForEach.Executar},
+                {"Usando Break - Estruturas de Controle", UsandoBreak.Executar},
+                {"Usando Continue - Estruturas de Controle", UsandoContinue.Executar},
             });
 
             central.SelecionarEExecutar();
